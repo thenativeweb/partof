d5c210768c017edd260ae0b1cfe019ba9986affe
Golo Roden <webmaster@goloroden.de>
2015-07-22 08:13:42 +0200

    Added implementation.

--- test/partOfTests.js
+++ test/partOfTests.js
@@ -9,4 +9,56 @@ suite('partOf', function () {
     assert.that(partOf).is.ofType('function');
     done();
   });
+
+  suite('returns true', function () {
+    test('if subset is part of superset.', function (done) {
+      var subset = { foo: 'a' },
+          superset = { foo: 'a', bar: 'b' };
+
+      assert.that(partOf(subset, superset)).is.true();
+      done();
+    });
+
+    test('if subset is part of superset, even for nested objects.', function (done) {
+      var subset = { bas: { baz: 'c' }},
+          superset = { foo: 'a', bar: 'b', bas: { baz: 'c' }};
+
+      assert.that(partOf(subset, superset)).is.true();
+      done();
+    });
+
+    test('if subset is part of superset, even for partially nested objects', function (done) {
+      var subset = { bas: { baz: 'c' }},
+          superset = { foo: 'a', bar: 'b', bas: { baz: 'c', bax: 'd' }};
+
+      assert.that(partOf(subset, superset)).is.true();
+      done();
+    });
+  });
+
+  suite('returns false', function () {
+    test('if subset is not part of superset.', function (done) {
+      var subset = { foo: 'b' },
+          superset = { foo: 'a', bar: 'b' };
+
+      assert.that(partOf(subset, superset)).is.false();
+      done();
+    });
+
+    test('if subset is not part of superset, even for nested objects.', function (done) {
+      var subset = { bax: { baz: 'c' }},
+          superset = { foo: 'a', bar: 'b', bas: { baz: 'c' }};
+
+      assert.that(partOf(subset, superset)).is.false();
+      done();
+    });
+
+    test('if subset is not part of superset, even for partially nested objects', function (done) {
+      var subset = { bax: { baz: 'c' }},
+          superset = { foo: 'a', bar: 'b', bas: { baz: 'c', bax: 'd' }};
+
+      assert.that(partOf(subset, superset)).is.false();
+      done();
+    });
+  });
 });
